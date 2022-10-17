d554473e1756f0284c81792dd1d96073ca44d5d6
Azhad <azhadh@gmail.com>
2022-10-17 16:24:05 +0800

    Expenses view

---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,14 @@
+import { Text, FlatList } from "react-native";
+import ExpenseItem from "./ExpenseItem";
+
+function renderItem(itemData) {
+    return (
+        <ExpenseItem {...itemData.item} />
+    );
+}
+
+function ExpensesList({expenses}) {
+    return <FlatList data={expenses} renderItem={renderItem} keyExtractor={(item) => item.id }  />
+}
+
+export default ExpensesList;
